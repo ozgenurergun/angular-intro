820c498e4366b9281b88b99c9e58436aa5e48b54
ozgenurergun <ozgenur.ergun@bil.omu.edu.tr>
2025-10-23 10:27:02 +0300

    .

--- src/app/interceptors/loader-interceptor.ts
+++ src/app/interceptors/loader-interceptor.ts
@@ -6,13 +6,13 @@ import { finalize } from 'rxjs';
 //bu bir fonksiyon. Class türü interceptorler de var.
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoaderService);
-  loadingService.setIsLoading(true);
+  loadingService.addRequest();
 
   //RxJS pipe
   return next(req).pipe(
     finalize(() => {
       console.log("İstek bitti.")
-      loadingService.setIsLoading(false);
+      loadingService.removeRequest();
     })
   );
 };
--- src/app/services/loader-service.ts
+++ src/app/services/loader-service.ts
@@ -6,7 +6,18 @@ import { Injectable, signal } from '@angular/core';
 export class LoaderService {
   isLoading = signal<boolean>(false);
 
-  setIsLoading(loading:boolean){
-    this.isLoading.set(loading);
+  private requestCount:number=0;
+
+  addRequest(){
+    this.requestCount++;
+    this.isLoading.set(this.requestCount > 0)
+  }
+
+  removeRequest(){
+    this.requestCount--;
+    if(this.requestCount < 0)
+      this.requestCount = 0
+
+    this.isLoading.set(this.requestCount > 0)
   }
 }
